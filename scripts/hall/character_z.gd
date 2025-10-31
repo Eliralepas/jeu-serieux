999f7090c162d7a156487c7c5cee12334d854573
Elif Meral <eliral.mrl67@gmail.com>
2025-10-31 22:16:10 +0100

    Ajout de dialogue (DialogBox) au niveau du narrateur

--- scripts/hall/character_z.gd
+++ scripts/hall/character_z.gd
@@ -1,9 +1,9 @@
-extends CharacterBody2D
+extends Node2D
 
 @onready var firt_time : bool = true
 
 func _ready() -> void:
 	if firt_time:
-		firt_time= false
+		$"../Control".visible = true
 		#dialog
 	pass
