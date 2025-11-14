b916ed74da7e9fbdf0d5ac5585360355b10fb603
Elif Meral <elif.meral@etu.unistra.fr>
2025-11-14 13:38:04 +0100

    Ajout de signaux pour chaque bouttons

--- scripts/menuPrinciaple/menu_principale.gd
+++ scripts/menuPrinciaple/menu_principale.gd
@@ -1,32 +1,34 @@
 extends Node2D
 
 @onready var Vbox : VBoxContainer = $Sprite2D/VBoxContainer
-const FILE_PATH  = "user://save_game.json"
+const FILE_PATH  = "res://save_game.json"
+
+@onready var button : Button = $Sprite2D/VBoxContainer/ButtonReprendre
 
 func _ready() -> void:
-	for i in range(Vbox.get_child_count()):
-		var button = Vbox.get_child(i)
-		
-		match i:
-			0:
-				button.pressed.connect(_on_jouer_pressed)
-			1:
-				var file = FileAccess.open(FILE_PATH, FileAccess.READ)
-				if file : 
-					button.disabled = false;
-				else :
-					button.disabled = true;
-				button.pressed.connect(_on_jouer_pressed)
-			2: 
-				button.pressed.connect(_on_credits_pressed)
-			3:
-				button.pressed.connect(_on_quitter_pressed)
+	var file = FileAccess.open(FILE_PATH, FileAccess.READ)
+	if file:
+		button.disabled = false
+		button.mouse_default_cursor_shape = Control.CURSOR_POINTING_HAND
+	else :
+		button.disabled = true
+		button.mouse_default_cursor_shape = Control.CURSOR_ARROW
 
-func _on_jouer_pressed() -> void:
+func _on_button_jouer_pressed() -> void:
+	var file = FileAccess.open(FILE_PATH, FileAccess.READ)
+	if file:
+		file.close()
+		DirAccess.remove_absolute(FILE_PATH)
 	get_tree().change_scene_to_file("res://scenes/hall.tscn")
 
-func _on_credits_pressed() -> void:
+
+func _on_button_reprendre_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/hall.tscn")
+
+
+func _on_button_credits_pressed() -> void:
 	print("Crédits")
 
-func _on_quitter_pressed() -> void:
+
+func _on_button_quitter_pressed() -> void:
 	get_tree().quit()
