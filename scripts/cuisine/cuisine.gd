f32ab366b1286ab51d282d4c574bff13136665d7
Elif Meral <elif.meral@etu.unistra.fr>
2025-12-04 06:53:00 +0100

    Ajout des emotions des personnages (assets et script)

--- scripts/cuisine/cuisine.gd
+++ scripts/cuisine/cuisine.gd
@@ -9,7 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for button_name in objects.keys():
-		var target_node = objects[button_name]
+		var target_node : Object_piece = objects[button_name]
 		
 		var check := CheckBox.new()
 		check.text = button_name
@@ -20,9 +20,10 @@ func _ready() -> void:
 		check.add_theme_color_override('font_disabled_color', Color.BLACK)
 		check.add_theme_color_override('font_hover_pressed_color', Color.BLACK)
 		check.button_pressed = target_node.visible
+		target_node.set_visibility(target_node.visible)
 		
 		v_box_container.add_child(check)
 		
 		check.toggled.connect(func(pressed: bool): 
-			target_node.visible = pressed
+			target_node.set_visibility(pressed)
 		)
