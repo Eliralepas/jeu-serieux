bfec137682d8b04a4007c3e41d87bc64a049082a
aymeric <aymeric.fenard@etu.unistra.fr>
2025-12-06 21:11:48 +0100

    Ajout des derniers assets, ne pas oublier d'ajouter les murs

---
+++ scripts/menu.gd
@@ -0,0 +1,28 @@
+extends Panel
+
+
+signal magasin_pressed
+signal finaliser_pressed
+
+
+func add_elem(btn: CheckButton) -> void:
+	if btn.get_type() == CheckButton:
+		$VBoxContainer.add_child(btn)
+		
+		
+func remove_elem(btn: CheckBox) ->void:
+	if btn.get_type() == CheckButton:
+		$VBoxContainer.remove_child(btn);
+		
+
+
+
+
+func _on_btn_magasin_pressed() -> void:
+	print("Bouton magasin pressé")
+	emit_signal("magasin_pressed")
+
+func _on_btn_finaliser_pressed() -> void:
+	print("Bouton finaliser pressé")
+
+	emit_signal("finaliser_pressed")
