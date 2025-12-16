9ccedd5d8d95287cdda5f2a6b983b110a36b679a
Elif Meral <elif.meral@etu.unistra.fr>
2025-12-16 23:00:39 +0100

    Revert "Restructure de la salle"
    This reverts commit dbc96d999cabda3cd4a2c62fefa905af0c65721f.

---
+++ scripts/cuisine/liste_objet.gd
@@ -0,0 +1,7 @@
+extends Node2D
+class_name ListeObjet
+
+func objectRandom() -> Object_piece:
+	var n: int = get_child_count()
+	var index_random: int = randi_range(0, n-1)
+	return get_child(index_random)
---
+++ scripts/cuisine/perso.gd
@@ -0,0 +1,65 @@
+extends AnimatedSprite2D
+
+class_name Personnage
+
+@export var ListeObjets : ListeObjet
+@onready var bulle : Sprite2D = get_node("Bulle")
+
+@onready var content: TextureRect = $Emotion/Content
+@onready var pas_content: TextureRect = $Emotion/PasContent
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if !ListeObjets:
+		push_error("%s : @export var ListeObjet est null" % name)
+	var object : Object_piece = ListeObjets.objectRandom()
+	var object_bulle : Sprite2D = Sprite2D.new()
+	object_bulle.texture = object.texture
+	var texture_object: TextureRect = get_node("Bulle/Objet")
+	resize(object_bulle, texture_object)
+	
+	var image_path: String
+	
+	
+	
+	
+	
+	if randi_range(0,1):
+		image_path = "res://assets/bulle/coeur.png"
+		object.aime.append(self)
+	else:
+		image_path = "res://assets/bulle/croix.png"
+		object.aimepas.append(self)
+	var image: Image = Image.new()
+	var error = image.load(image_path)
+	if error != OK:
+		push_error("Erreur")
+		return
+	var avis = ImageTexture.create_from_image(image)
+	var avis_sprite : Sprite2D = Sprite2D.new()
+	avis_sprite.texture = avis
+	var texture_avis: TextureRect = get_node("Bulle/Avis")
+	resize(avis_sprite, texture_avis)
+	
+	bulle.visible = false
+
+func resize(sprite : Sprite2D, texture : TextureRect) :
+	var size : Vector2 = sprite.texture.get_size()
+	var x: float = size.x
+	var y: float = size.y
+	var scale_factor: float = min(x / texture.size.x, y / texture.size.y)
+	sprite.scale = Vector2.ONE / scale_factor
+	texture.add_child(sprite)
+	sprite.position = texture.size/2
+
+func _on_button_pressed() -> void:
+	bulle.visible = not bulle.visible
+	pass # Replace with function body.
+
+func visible_emotion(estcontent : bool) -> void:
+	if estcontent :
+		content.visible = true
+		pas_content.visible = false
+	else :
+		content.visible = false
+		pas_content.visible = true
