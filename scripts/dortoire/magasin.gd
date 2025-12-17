74723cec68c7bd09347f8b2f39a5c578d3aa1ffd
Fella Bennounas <bennounas@unistra.fr>
2025-12-17 14:15:49 +0100

    correction du bug objet visible/checkbutton non activé

---
+++ scripts/dortoire/magasin.gd
@@ -0,0 +1,231 @@
+extends CheckBTN
+
+@onready var scene_cadre: PackedScene = preload("res://scenes/dortoire/cadre.tscn")
+
+@onready var parle = $dialogue
+@onready var area = $area2D
+@onready var h_flow_container: HFlowContainer = $hFlowContainer
+
+#@onready var caisse:= $songs/purchase 
+
+@onready var budget
+@onready var objets_achetes=0;
+
+@onready var test=get_parent().get_node("menu/panel/vBoxContainer")
+
+@onready var stock = get_parent().get_stock()
+@onready var stuff
+			#les differentes phrases
+var dialogues = [
+	"Me dévalise pas ! Pas plus de quatres articles.\nOn est au Pole Sud ici !",
+	"Qu'est-ce que tu veux ?",
+	"Accélère, j'ai d'autres clients !" # dernière phrase normale
+]
+
+		#objets du magasin
+		#tab de listes["nom", prix]
+		#! chaque piece a des objets differents
+var objets = [
+	["Cadres",20],
+	["Armoire",120],
+	["Lampe",32],
+	["Plante1",14],
+	["Plante2",17],
+	["Pouf",55],
+	["Commode",67]
+	]
+
+var index = 0 #pour avoir le nb de clicks sur le dialogue
+var nb_objet=0 #pour compter les objets deja achetes
+
+#####################################################################################
+
+
+func _ready():
+	print("///////////////////////////////////////////////////////////")
+	await get_tree().process_frame #temps pour que le parent fasse son ready
+	stuff = get_parent().get_objects()  # récupère ici
+	parle.text = dialogues[index]
+	#area.input_event.connect(_on_area_input_event)
+	remplir_magasin()
+	
+	# connecte les boutons du store
+	#$Btn_Acheter.connect("pressed", Callable(self, "_on_button_acheter_pressed"))
+	#$Btn_Sortir.connect("pressed", Callable(self, "_on_btn_sortir_pressed"))
+
+###########################################################################
+func ajouter_obj_au_magasin(nom: String, prix: int) -> void: #si on voudra ajouter un item au magasin apres 
+	var element = [nom, prix] # pas necessairement utile mais elle existe 
+	objets.append(element)
+###########################################################################
+func remplir_magasin() -> void:
+	#remplir le magasin avec les différents objets (prix, button, image, nom)
+	for obj in objets:
+		var cadre : CadreObjet = scene_cadre.instantiate()
+		cadre.name = "GRILLE" + obj[0]
+		
+		#Ajout de l'image (le chemin a changé si besoin)
+		var texture_objet = cadre.getObjet()
+		var image_path : String = "res://assets/dortoir/meubles/"+obj[0]+".png"
+		var image: Image = Image.new()
+		var error = image.load(image_path)
+		if error != OK:
+			push_error("Erreur")
+			return
+		var texture = ImageTexture.create_from_image(image)
+		texture_objet.texture = texture
+		
+		var sprite = cadre.getCadre()
+		
+		# changer la taille de l'image de l'objet
+		var size : Vector2 = sprite.texture.get_size()
+		var x: float = size.x - 6
+		var y: float = size.y - 6
+		var image_size : Vector2 = texture.get_size()
+		
+		# Calcul du facteur d'échelle
+		var scale_factor: float = min(x / image_size.x, y / image_size.y)
+		texture_objet.scale = Vector2.ONE * scale_factor
+		
+		#Ajout le nom de l'objet
+		var objet_check = cadre.getCheckButton()
+		objet_check.text = obj[0]
+		objet_check.disabled = false
+		
+		#Ajout du label prix
+		var objet_prix = cadre.getLabel()
+		objet_prix.text = str(obj[1]) + " $"
+		
+		#on connecte chaque checkbox a la fct _check_a_checkbox lorsque elle est "toggled" (cochee)
+		objet_check.connect("toggled", Callable(self, "_check_a_checkbox").bind(objet_check))
+		
+		h_flow_container.add_child(cadre)
+
+	#LA FCT QUI GERE LE MAX: 4 CHECKBOX CHOISIES		
+func _check_a_checkbox(button_pressed: bool, toggled_checkbox: CheckBox) -> void:
+	var checked_count = 0 #compteur de nb de checkbox deja cliquee
+	
+	for obj in objets: #parcours de la liste d'objets
+		var gr : CadreObjet = h_flow_container.get_node("GRILLE" + obj[0]) #on recup la grille (image+checkbox)
+		var check = gr.getCheckButton() #dans chaque grille on recup juste la checkbox
+		if check.button_pressed: #si la checkbox est click on incremente checked_count
+			checked_count += 1
+	
+	if checked_count > 4 and button_pressed: #si au final il y'a deja 4
+		toggled_checkbox.button_pressed = false
+
+
+#############################################################################
+
+func _on_btn_sortir_pressed() -> void:
+	decoche_tout()
+	magasin_non_visible()
+
+	#LORSQUE LE BOUTON ACHETER EST CLICK
+	
+func _on_button_acheter_pressed() -> void:
+	var total = 0
+	var checked_objects = []
+	
+	# Récupère tous les objets cochés
+	for obj in objets: #recupere le prix final
+		var gr : CadreObjet = h_flow_container.get_node("GRILLE" + obj[0]) #recup grille de chaque obj
+		var check = gr.getCheckButton() #recup checkbox
+		if check.button_pressed: #si obj choisi
+			total += obj[1]#ajout du prix au total
+			checked_objects.append(obj)#ajout de l'objet a la liste d'objets
+			
+			
+	if (objets_achetes + checked_objects.size()) > 4: #verifie qu'on ne veut pas plus de 4 objets
+		parle.text = "On a dit uniquement  4 objets!"
+
+	# Vérifie si le budget suffit
+	elif total <= budget: 
+		var any_added = false
+		for obj in checked_objects:
+			var ajoutee = get_parent().ajoute_objet(obj[0], stock)
+			if ajoutee:
+				any_added = true
+			else:
+				objets_achetes -= 1 # On décrémente le nombre d'objet
+				total -= obj[1] # On retire le prix de l'objet déjà possédé au résultat
+				parle.text = "Tu l'as déjà, je ne vends pas en double."
+
+		if any_added:
+			budget -= total  # soustraction globale ici
+			objets_achetes += checked_objects.size()  # ← correction ici
+			print(objets_achetes)
+			parle.text = "Hahaha j'aime quand tu depenses."
+
+			#caisse.play()
+			await get_tree().create_timer(1.1).timeout
+
+			magasin_non_visible()
+			get_parent().clear_check_boxes()
+			get_parent().add_check_button(stock, stuff, test)
+			get_parent().reconnect_menu_buttons()
+			get_parent().connect_the_check_boxs(stuff)
+			get_parent().set_budget(budget)
+			
+			
+
+	else:
+		parle.text = "Haha dans tes rêves, t'as pas l'argent"
+
+
+
+
+	#utilise pour que lorsq'on revient dans le magasin les checkbox d'avant ne seront plus cochees
+func decoche_tout() -> void:
+	for obj in objets: #parcours de la liste d'objets
+		var gr : CadreObjet = h_flow_container.get_node("GRILLE" + obj[0]) #on recup la grille (image+checkbox)
+		var check = gr.getCheckButton() #dans chaque grille on recup juste la checkbox
+		check.disabled = false 
+		if check.button_pressed: #si la checkbox est click on incremente checked_count
+			check.button_pressed=false
+
+
+func magasin_non_visible() -> void:
+# récupère le node du magasin explicitement
+	var magasin_node = get_parent().get_node("store")  
+	magasin_node.visible = false
+
+	var parent = get_parent()
+	var backgroundMagasin = parent.get_node("songs/magasinBackground")
+	var people = parent.get_node("songs/talkingPeople")
+	var mainbackground = parent.get_node("songs/mainBackground")
+	#var argent = parent.get_node_or_null("Menu/Panel/Label")
+
+	backgroundMagasin.stop()
+	people.stop()
+	mainbackground.play()
+	
+	get_parent().get_node("menu").change_budget(budget);
+
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed: #si l'evenemt est un clique
+		index += 1 #l'indice augmente de 1
+
+		#phrase secrete
+		if index == 10:
+			parle.text = "T'as fini ?!"
+			return
+		
+		if index == 20:
+			parle.text = "Juste vas t'en..."
+			return
+		
+		if index == 30:
+			parle.text = "DEGAGEEEEEE !"
+			parle.add_theme_font_size_override("font_size", 60)
+			return
+		
+		# Plus de 3 clics → reste sur la dernière phrase
+		if index > 2:
+			parle.text = dialogues[2]
+			parle.add_theme_font_size_override("font_size", 30)
+		#comportement normal
+		else:
+			parle.text = dialogues[index]
+	pass # Replace with function body.
---
+++ scripts/dortoire/piece.gd
@@ -0,0 +1,235 @@
+extends CheckBTN
+
+class_name BasePiece
+
+#les attributs que chaque piece doit avoir: 
+#(adaptez les chemins si besoin)
+
+# pour la gestion du JSON (adaptez les chemins si besoin)
+const PATH : String = "res://save_game.json"
+const NOM_SALLE : String = "dortoir"
+
+@onready var porte= $songs/porte
+@onready var magasinBackground = $songs/magasinBackground
+@onready var talkingPeople=$songs/talkingPeople
+@onready var mainBackground=$songs/mainBackground
+
+@onready var broken_object: ObjetCasse = $brokenObject
+@onready var btn_reparer: Button = $brokenObject/objetCasse/btnReparer
+@onready var objet_casse: Button = $brokenObject/objetCasse
+
+@onready var menu = $menu #un menu pour poser/retirer des objets
+@onready var store= $store #chaque piece a un magasin
+
+@onready var conteneur=$menu/panel/vBoxContainer
+@onready var objects := { #tous les elements possible dans la piece(donc achetable depuis le magasin)
+	#"nom": $cheminImage,
+	"Cadres": $listeObjets/Cadres,
+	"Armoire":$listeObjets/Armoire,
+	"Lampe":$listeObjets/Lampe,
+	"Plante1":$listeObjets/Plante1,
+	"Plante2":$listeObjets/Plante2,
+	"Pouf":$listeObjets/Pouf,
+	"Commode":$listeObjets/Commode,
+
+}
+
+@export var Personnages : Node2D
+
+var budget := 0 : #A lire depuis le Json
+	set(val) :
+		budget = val
+		$store.budget = val
+		print(budget)
+		
+var stock :Array= [] #Les objets qu'on a (soit des qu'on entre dans la piece soit qu'on achete du magasin)
+						#JUSTE LE NOM
+
+func _ready() -> void:
+	setup();
+
+func get_stock() ->Array:
+	return stock
+
+func get_objects() -> Dictionary:
+	return objects
+	#cette fonction pourra etre utiliser dans tous les ready
+func setup() -> void:
+	add_check_button(stock, objects, conteneur) #on lui donne ce qu'on a et TOUS les objets possibles
+	connect_the_check_boxs(objects) #on lui donne tous les objets possible, elle gere pour trier
+	for obj : ObjectPiece in objects.values():
+		obj.set_visibility(false)
+		
+
+		
+	$menu._on_menu_mur_color_pressed()
+	
+	var file = FileAccess.open(PATH, FileAccess.READ)
+	if file :
+		var content = file.get_as_text()
+		file.close()
+		var json = JSON.parse_string(content)
+		budget = json["budget"]
+		$menu.change_budget(budget)
+	else :
+		push_error("Erreur ouverture du JSON")
+	
+		#on connecte les 2 btn du menu auc fct qui leurs sont attribuer
+	$menu/panel/btnMagasin.connect("pressed", Callable(self, "_magasin_pressed"))
+	$menu/panel/btnFinaliser.connect("pressed", Callable(self, "_finaliser_pressed"))
+	
+		#on connecte le petit menu mur a sa fct
+	$menu/panel/menuMurColor.get_popup().connect("id_pressed", Callable(self, "_on_mur_color_selected"))
+	
+		#the magasin
+	$store/btnAcheter.connect("pressed", Callable(self, "_on_button_acheter"))
+	$store/btnSortir.connect("pressed", Callable(self, "_on_btn_sortir"))
+
+
+	#cette fonction est utile si on a un stock par defaut 
+func default_stock(stk: Array)->void:
+	stock=stk
+
+func ajout_obj(obj: Dictionary) -> void:
+	for key in obj.keys():
+		stock.append(obj[key]) 
+
+func _magasin_pressed():
+	$menu._on_magasin_pressed($store,porte, magasinBackground, talkingPeople, mainBackground)
+	
+	await get_tree().create_timer(1.5).timeout
+	for perso : Personnage in Personnages.get_children() :
+		perso.visible = false
+	objet_casse.visible = false
+	btn_reparer.visible = false
+
+func _finaliser_pressed():
+	_calcul_score()
+	$menu._on_finaliser_pressed()
+
+# on calculer un certain score qu'on le stocke dans le json
+# | 	les besoins humains : 40%
+# | 	l'adaptation au saison : 25%
+# | 	la réparation d'un objet : 15%
+# | 	choix de la bonne couleur du mur : 20%
+func _calcul_score() ->void :
+	#on vérifie si tout d'abord nous avons écouter l'avis des résidents (40%)
+	#pour chaque résident satisfait on donne +1
+	var remarques : String = ""
+	var nbContent : int = 0
+	for perso : Personnage in Personnages.get_children() :
+		if perso.is_content() :
+			nbContent = nbContent + 1
+	
+	if nbContent == 3 : 
+		remarques += "Et bien, tout les résidents ont été satisfait !\n"
+	elif nbContent == 2:
+		remarques += "On ne peut pas satisfaire tout le monde.\n"
+	elif nbContent == 1 : 
+		remarques += "Tu aurais pu faire mieux, j'ai eu deux plaintes.\n"
+	else : 
+		remarques += "Bon là... Tu fais aucun effort, tout le monde est de mauvaise humeur.\n"
+	
+	#Avec 3 résidents = 3 points max, on normalise
+	var scoreTotal : float = 0
+	scoreTotal += nbContent/3.0 * 4.0
+	
+	#si l'objet a été réparé (15%)
+	var repare : bool = broken_object.est_repare()
+	if repare :
+		scoreTotal += 1.5
+		remarques += "C'est bien que tu aies pu réparer l'objet cassé, merci.\n"
+	else : 
+		remarques += "Ça aurait été bien de pouvoir réparer cet objet.\n"
+	
+	#vérifier si la bonne couleur de mur a été choisie (20%)
+	var murs : Mur = menu.murs
+	if murs.bonne_couleur_choisie() :
+		scoreTotal += 2
+		remarques += "D'ailleurs, la couleur du mur plait à tout le monde, pas trop clair ni trop foncé.\n"
+	else :
+		remarques += "Bon, au niveau de la couleur du mur tu aurais pu faire plus d'effort.\n"
+	print(murs.bonne_couleur_choisie())
+	
+	#pour le calcul de l'adaptation au saison (on récupère la saison du json)
+	var file = FileAccess.open(PATH, FileAccess.READ_WRITE)
+	if file :
+		var content = file.get_as_text()
+		var json = JSON.parse_string(content)
+		
+		if json!=null:
+			# 0 si été, 1 si hiver
+			var saison : int = json[NOM_SALLE]["saison"]
+			if saison == 0 : #si été
+				#vérifier si le rideau est visible
+				var rideau = objects["radiateur"]
+				if rideau.visible : 
+					scoreTotal += 2.5
+					remarques += "C'est une superbe idée d'avoir mis les rideaux, puisqu'il fait tout le temps jour durant cette saison.\n"
+				else : 
+					remarques += "Durant cette saison, il fait tout le temps jour...Des rideaux n'auraient fait de mal à personne.\n"
+			elif saison == 1 : 
+				#vérifier si la lampe/lumière est visible
+				var lampe = objects["cd"]
+				if lampe.visible : 
+					scoreTotal += 2.5
+					remarques += "C'est une superbe idée d'avoir mis la lampe, il fait tout le temps nuit durant cette saison.\n"
+				else : 
+					remarques += "Durant cette saison, il fait tout le temps nuit...On aurait aimé voir plus de lumière.\n"
+				
+			json["budget"] = budget
+			json[NOM_SALLE]["remarques"] = remarques
+			json[NOM_SALLE]["score"] = scoreTotal
+			json["tour"] += 1
+		_change_json(json)
+		file.close()
+	else : 
+		print("Erreur sur la lecture du fichier json.")
+		push_error("JSON")
+
+func _change_json(json) :
+	var file_write := FileAccess.open(PATH, FileAccess.WRITE)
+	if file_write:
+		file_write.store_string(JSON.stringify(json, "\t", true))
+		file_write.close()
+	else:
+		push_error("Impossible d'écrire le fichier JSON.")
+
+func set_budget(_budget) :
+	budget = _budget
+
+func reconnect_menu_buttons():
+	var btnMag = $menu/panel/btnMagasin
+	var btnFin = $menu/panel/btnFinaliser
+
+	if not btnMag.is_connected("pressed", Callable(self, "_magasin_pressed")):
+		btnMag.connect("pressed", Callable(self, "_magasin_pressed"))
+
+	if not btnFin.is_connected("pressed", Callable(self, "_finaliser_pressed")):
+		btnFin.connect("pressed", Callable(self, "_finaliser_pressed"))
+		
+func _on_button_acheter() :
+	for perso : Personnage in Personnages.get_children() :
+		perso.visible = true
+	objet_casse.visible = true
+	$store._on_button_acheter_pressed()
+	
+func _on_btn_sortir() :
+	for perso : Personnage in Personnages.get_children() :
+		perso.visible = true
+	objet_casse.visible = true
+	$store._on_btn_sortir_pressed()
+
+func _on_btn_reparer_pressed() -> void:
+	var cout : int = broken_object.get_cout()
+	if cout > budget :
+		print("cout trop élévé")
+		return
+	
+	%animationRepare.play("reparer")
+	btn_reparer.visible = false
+	objet_casse.disabled = true
+	broken_object.set_repare()
+	objet_casse.mouse_default_cursor_shape = Control.CURSOR_ARROW
+	budget-=cout
+	$menu.change_budget(budget)
